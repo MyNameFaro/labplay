e9b52e7f8709460a886eb98d127e3fd226928f43
MyNameFaro <57633849+MyNameFaro@users.noreply.github.com>
2019-12-08 23:01:52 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿Vue.component('a-button', {
+	template: '<a class="a_button"><slot></slot></a>'
+});
+Vue.component('menu-bar', {
+	template: '<div class="menu-bar"><h4><slot></slot></h4></div>'
+});
+Vue.component('a-link', {
+	template: '<a><h1><slot></slot></h1></a>'
+});
+Vue.component('level-right', {
+	template: '<div class="level-right"><slot></slot></div>'
+});
+Vue.component('search', {
+	template: '<form class="search"><input type="textbox" placeholder="ค้นหา"></form>'
+});
+Vue.component('h-title',{
+	template: '<h1 class="title"><slot></slot></h1>'
+});
+Vue.component('content', {
+	template: '<div class="content"><slot></slot></div>'
+});
+new Vue({ 
+	el: '#app'
+});
